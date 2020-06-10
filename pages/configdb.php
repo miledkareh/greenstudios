3cb4c6ede664369baebf7cd0930d8c0eafa31dcd
Alex <alexandre.bitar@isae.edu>
2020-06-10 13:55:01 +0300

    fixed untracked files

--- pages/configdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbhandle = mysqli_connect("localhost","root","djGj5DAzFChLpm") 
-  or die("Unable to connect to MySQL");
-$selected = mysqli_select_db($dbhandle,"greenstudios") 
-  or die("Could not select greenstudios");
-date_default_timezone_set("Asia/Beirut");
-?>
